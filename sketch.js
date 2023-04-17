472d2bc301c65e261e11e85d567860d5f7f23895
Jan5000 <60040649+Jan5000@users.noreply.github.com>
2023-04-17 19:35:20 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
-let s = 1;
+let s = 0;
 let c;
 let newCircle;
 let tapped = false;
 let song;
+let arrayXY = [];
+let stepSize;
 
 function preload() {
   song = loadSound('assets/why421 - warm beer.mp3');
@@ -19,29 +21,54 @@ function draw() {
   
 
   if (mouseIsPressed === true) {
+    noFill();
     tapped = true;
     s++;
     background(0,0,0,15);
     //c = color();
-    stroke(255,255,255);
+    stroke(map(mouseX, 0, width, 200, 255),map(mouseX, 0, width, 255, 200),map(mouseY, 0, height, 255, 200));
     ellipse(mouseX, mouseY, 100+abs(sin(s/50)*25), 100+abs(sin(s/50)*25));
+
+    arrayXY[s] = [round(mouseX), round(mouseY)];
+    console.log(arrayXY[s]);
+    //console.log(arrayXY.length);
+    s++;
   }
   else {
     if(tapped) {
       tapped = false;
-      background(55);
+      console.log(arrayXY);
+      background(0);
+      stroke(255,255,255);
+      //beginShape();
+      stepSize = width/arrayXY.length;
+      for(let i = 1; i < arrayXY.length; i = i+2) {
+        //fill(255);
+        noStroke();
+        fill(map(arrayXY[i][0], 0, width, 100, 255),map(arrayXY[i][0], 0, width, 255, 100),map(arrayXY[i][1], 0, height, 255, 100));
+        ellipse(stepSize*i,arrayXY[i][0],10,10);
+        ellipse(stepSize*i,arrayXY[i][1],10,10);
+        //vertex(arrayXY[i][0],arrayXY[i][1]);
+      }
+      //noFill();
+      //stroke(255);
+      //strokeWeight(1);
+      //endShape();
     }
-    s = 1;
-    background(0,0,0,15);
+    s = 0;
+    //background(0,0,0,15);
     //ellipse(mouseX, mouseY, 75, 75);
   }
+
+
+
 }
 
 function touchEnded() {
   song.stop();
-  console.log("ww");
+  //console.log("ww");
 }
 function touchStarted() {
   song.play();
-  console.log("111");
+  //console.log("111");
 }
