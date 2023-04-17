39417ea746eb058e855169f7f1bf400377f428c6
Jan5000 <60040649+Jan5000@users.noreply.github.com>
2023-04-17 18:29:56 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,48 @@
 let s = 1;
+let c;
 let newCircle;
+let tapped = false;
+let song;
+
+function preload() {
+  song = loadSound('assets/why421 - warm beer.mp3');
+}
 
 function setup() {
+  
   c = createCanvas(windowWidth, windowHeight);
   background(0);
   noFill();
-  stroke(255);
   strokeWeight(3);
-  filter(GRAY);
 }
 
 function draw() {
   
 
-  /* if(mouseIsPressed === true) {
-    newCircle = new tapCircle(100, 100, 100, 100);
-    newCircle.display();
-  }
-  else {} */
-
   if (mouseIsPressed === true) {
+    tapped = true;
     s++;
     background(0,0,0,15);
-    ellipse(mouseX, mouseY, 100, 100);
-  } else {
+    //c = color();
+    stroke(255,255,255);
+    ellipse(mouseX, mouseY, 100+abs(sin(s/50)*25), 100+abs(sin(s/50)*25));
+  }
+  else {
+    if(tapped) {
+      tapped = false;
+      background(55);
+    }
     s = 1;
-    background(0);
+    background(0,0,0,15);
     //ellipse(mouseX, mouseY, 75, 75);
   }
 }
 
-class tapCircle {
-  constructor(posX,posY,cwidth,cheight) {
-    this.posX = posX;
-    this.posY = posY;
-    this.cwidth = cwidth;
-    this.cheight = cheight;
-  }
-  //cS = 1;
-  display() {
-    //cS++;
-    ellipse(this.posX, this.posY, this.cwidth, this.cheight);
-  }
+function mouseReleased() {
+  song.stop();
+  console.log("ww");
 }
-
-
 function mousePressed() {
-  ellipse(10 , 10, 5, 5);
-  // prevent default
-  return false;
+  song.play();
+  console.log("111");
 }
